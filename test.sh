9af8e2864c3c75753aa287925e839648d6e4a159
hadar-co <hadar@datree.io>
2022-08-03 16:49:54 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,9 @@
 set -x
 
 PASSED=$(jq .policySummary.totalPassedCount blah.json)
+FAILED=$(jq .policySummary.totalFailedCount blah.json)
 
+echo "Datree policy check results:  " >> $GITHUB_STEP_SUMMARY
 echo "| Total rules | Passed | Failed |" >> $GITHUB_STEP_SUMMARY
 echo "|-------------|--------|--------|" >> $GITHUB_STEP_SUMMARY
-echo "| 33333 | $PASSED | asdfasdf |" >> $GITHUB_STEP_SUMMARY
+echo "| 33333 | $PASSED | $FAILED |" >> $GITHUB_STEP_SUMMARY
