26aaa3b1fa3704fc79e0b3236d13084ca8888bae
hadar-co <hadar@datree.io>
2022-08-09 11:43:26 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,7 +8,8 @@ PASSED=$(jq .policySummary.totalPassedCount blah.json)
 FAILED=$(jq .policySummary.totalRulesFailed blah.json)
 
 echo "<img src=\"https://raw.githubusercontent.com/datreeio/datree/main/images/datree-logo.svg\" width=\"180\"/>&nbsp;" >> $GITHUB_STEP_SUMMARY
-echo "<div align=\"center\">## Datree policy check results</div>&nbsp;" >> $GITHUB_STEP_SUMMARY
+echo "⭐ Want protection on your cluster? Try out our [admission webhook!](https://github.com/datreeio/webhook-datree) ⭐" >> $GITHUB_STEP_SUMMARY
+echo "## Datree policy check results" >> $GITHUB_STEP_SUMMARY
 echo "**Policy name:** $POLICY_NAME" >> $GITHUB_STEP_SUMMARY
 echo "| Total rules | Passed | Failed |" >> $GITHUB_STEP_SUMMARY
 echo "|-------------|--------|--------|" >> $GITHUB_STEP_SUMMARY
